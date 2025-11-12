fc8c32ba9ed4b671f5f502d036ace665d964abf4
RenXusheng233 <782449909@qq.com>
2025-11-12 10:01:47 +0800

    feat: Navbar

---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,39 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import SearchBar from './Searchbar'
+import { Bell, Home, ShoppingCart } from 'lucide-react'
+
+const Navbar = () => {
+  return (
+    <nav className="w-full flex items-center justify-between border-b border-gray-200 pb-4">
+      {/* Left */}
+      <Link href="/" className="flex items-center">
+        <Image
+          src="/logo.png"
+          alt="TrendRR"
+          width={36}
+          height={36}
+          className="w-6 h-6 md:w-9 md:h-9"
+        />
+        <p className="hidden md:block text-lg font-medium tracking-wider">
+          TRENDRR.
+        </p>
+      </Link>
+      {/* Right */}
+      <div className="flex items-center gap-6">
+        <SearchBar />
+        <Link href="/">
+          <Home className="w-4 h-4 text-gray-600" />
+        </Link>
+        <Bell className="w-4 h-4 text-gray-600" />
+        <ShoppingCart className="w-4 h-4 text-gray-600" />
+        {/* TODO: login page */}
+        <Link href="/login" className="hover:bg-gray-50 rounded-sm px-0.5">
+          Sign in
+        </Link>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
